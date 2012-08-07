f9ed47ff950675b26a5c96fc560a2d72b2b56074
Andrea Cantieni PHZ <andrea.cantieni@phz.ch>
2012-08-07 10:54:41 +0200

    init

---
+++ sidebar.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The Sidebar containing the main widget area.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+$options = twentyeleven_get_theme_options();
+$current_layout = $options['theme_layout'];
+
+if ( 'content' != $current_layout ) :
+?>
+		<div id="secondary" class="widget-area box-right-copingblog" role="complementary">
+			<aside id="wp_sidebarlogin" class="widget">
+				<?php the_widget('SidebarLoginMultiWidget',NULL,array('before_title'=>'<h3 class="widget-title">', 'after_title'=>'</h3>'))?>
+			</aside>
+			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+				
+				<aside id="archives" class="widget">
+					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</aside>
+
+				<aside id="meta" class="widget">
+					<h3 class="widget-title"><?php _e( 'Meta', 'twentyeleven' ); ?></h3>
+					<ul>
+						<?php wp_register(); ?>
+						<li><?php wp_loginout(); ?></li>
+						<?php wp_meta(); ?>
+					</ul>
+				</aside>
+
+			<?php endif; // end sidebar widget area ?>
+		</div><!-- #secondary .widget-area -->
+<?php endif; ?>
